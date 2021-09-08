6ecd2a26d3eac76c8587ca36a6a9489f3248c4b0
SofiaAkbar <sofiaakbar@hotmail.co.uk>
2021-09-08 12:09:37 +0100

    delete path working

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -31,6 +31,36 @@ const createRouter = function(collection) {
             });
     })
 
+    // CREATE NEW
+    router.post('/', (req, res) => {
+        const newData = req.body;
+        collection
+            .insertOne(newData)
+            .then((result) => {
+                res.json(result.ops[0])
+            })
+            .catch((err) =>{
+                console.error(err);
+                res.status(500);
+                res.json({ status: 500, error: err})
+            });
+    })
+
+    // DELETE 
+    router.delete('/:id', (req, res) => {
+        const id = req.params.id;
+        collection
+            .deleteOne({ _id: ObjectId(id)})
+            .then(() => collection.find().toArray())
+            .then((docs) => res.json(docs))
+            .catch((err) =>{
+                console.error(err);
+                res.status(500);
+                res.json({ status: 500, error: err})
+            });
+    })
+
+
 
 
 
